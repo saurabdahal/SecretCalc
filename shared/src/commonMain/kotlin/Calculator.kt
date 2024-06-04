b10433d7b777afc60f1b99da930259e9ac47abf3
saurav dahal <d.saurab1@gmail.com>
2024-06-04 19:59:49 -0400

    created calculator project and added Add method

---
+++ shared/src/commonMain/kotlin/Calculator.kt
@@ -0,0 +1,17 @@
+/*
+Simple Class that defines methods to perform basic Arithmetic calculations
+ */
+class Calculator {
+
+    /**
+     * Adds two integers and returns the result.
+     * @param left The first integer to add.
+     * @param right The second integer to add.
+     * @return The sum of the two integers.
+     */
+    fun Add(left: Int, right: Int): Int {
+        return left + right
+    }
+
+
+}
