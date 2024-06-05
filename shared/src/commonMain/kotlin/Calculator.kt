bf60b1ca7386b60b604c83ba1ab44fd2c1780487
saurav dahal <d.saurab1@gmail.com>
2024-06-04 20:00:56 -0400

    feat: Added divide method

--- shared/src/commonMain/kotlin/Calculator.kt
+++ shared/src/commonMain/kotlin/Calculator.kt
@@ -33,5 +33,21 @@ class Calculator {
         return left * right
     }
 
+    /**
+     * Performs division operation between two parameters
+     * @exception IllegalArgumentException : catches exception if second parameter is Zero
+     * @param left The integer to be divided.
+     * @param right The integer to divide.
+     * @return The quotient of the two integers.
+     */
+    fun Divide(left: Int, right: Int): Int {
+        var result: Int = 0
+        try {
+            result = left / right
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
 
+        return result
+    }
 }
