64abdd7730873a95cf94dcb7e696af6a2e3e7ed2
saurav dahal <d.saurab1@gmail.com>
2024-06-11 17:26:59 -0400

    changed package name

--- composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.multicalculator
+package org.example.multicalculator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,16 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
